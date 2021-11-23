054d19008cef7cb63dbf84e914ab60462f8b3004
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-11-22 23:08:16 -0600

    difference insurance cost function

--- insurance_ex.py
+++ insurance_ex.py
@@ -4,6 +4,10 @@ def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker, name):
   print("The estimated insurance cost for " + name + " is "+ str(estimated_cost) + " dollars.")
   return estimated_cost
 
+def difference_insurance_cost(person1, person2):
+  difference_cost = person1 - person2
+  print("The difference in insurance cost is " + str(difference_cost) + " dollars.")
+
 # Estimate Maria's insurance cost
 
 maria_insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0, "Maria") 
@@ -13,3 +17,6 @@ omar_insurance_cost = calculate_insurance_cost(35, 1, 22.2, 0, 1, "Omar")
 
 # Estimate Carlos's insurance cost
 carlos_insurance_cost = calculate_insurance_cost(26, 1, 23.4, 0, 0, "Carlos")
+
+# Function to calculate the difference in insurance cost between two persons
+difference_insurance_cost(maria_insurance_cost, carlos_insurance_cost)
